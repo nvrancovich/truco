e51c4cc8348542b70026cd4f60a61bd54fce2123
nvrancovich <nvrancovich@gmail.com>
2022-02-12 10:03:45 -0300

    Add score function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import I
 import time
 import random
 
@@ -12,4 +13,17 @@ def shuffle(deck, hand):
     time.sleep(1)
     print(random.sample(deck, hand))
 
+def score(player_score, oponent_score):
+    player_score_list = list()
+    oponent_score_list = list()
+    for i in range (0, player_score):
+        player_score_list.append('/')
+    for i in range (0, oponent_score):
+        oponent_score_list.append('/')
+    player_score = ''.join(i for i in player_score_list)
+    oponent_score = ''.join(i for i in  oponent_score_list)
+    print(f'Yo: {player_score}')
+    print(f'El: {oponent_score}')
+
 shuffle(CARDS, 3)
+score(4, 3)

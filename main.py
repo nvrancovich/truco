a5df5796f086fafb42e516954db267c199a916e7
nvrancovich <nvrancovich@gmail.com>
2022-02-14 09:26:53 -0300

    Add envido function and change card deck

--- main.py
+++ main.py
@@ -3,26 +3,42 @@ import random
 import os
 
 from cards import CARDS
+from envido_values import ENVIDO_VALUES
 
 def clear(): 
     os.system('clear')
 
+def envido(first_hand, second_hand):
+    first_envido = ENVIDO_VALUES[first_hand[0]] + ENVIDO_VALUES[first_hand[1]] + ENVIDO_VALUES[first_hand[2]]
+    second_envido = ENVIDO_VALUES[second_hand[0]] + ENVIDO_VALUES[second_hand[1]] + ENVIDO_VALUES[second_hand[2]]
+    return first_envido, second_envido
+
 def hand(player_first, oponent_first, hand1, hand2):
     if player_first:
         #First round
-        player_first_round = int(input(f'Elegí la carta que vas a tirar 1) {hand1[0]} 2) {hand1[1]} 3) {hand1[2]}: '))
-        hand1.remove(hand1[player_first_round - 1])
+        player_first_round = int(input(f'Tu turno: 1) {hand1[0]} 2) {hand1[1]} 3) {hand1[2]} 4) Cantar Envido: '))
+        if player_first_round == 4:
+            envido_player, envido_oponent = envido(hand1, hand2)
+            print('Yo: Envido!')
+            print('Él: Quiero')
+            print(f'Yo: {envido_player}')
+            if envido_oponent > envido_player:
+                print(f'Él: {envido_oponent} son mejores')
+            else:
+                print('Él: Son buenas')
+        else:
+            hand1.remove(hand1[player_first_round - 1])
         oponent_first_round = random.randint(0,2)
         print(f'Él tira un {hand2[oponent_first_round]}!')
         hand2.remove(hand2[oponent_first_round])
         #Second round
-        player_second_round = int(input(f'Elegí la carta que vas a tirar 1) {hand1[0]} 2) {hand1[1]}: '))
+        player_second_round = int(input(f'Tu turno: 1) {hand1[0]} 2) {hand1[1]}'))
         hand1.remove(hand1[player_second_round - 1])
-        oponent_second_round = random.randint(0,2)
+        oponent_second_round = random.randint(0,1)
         print(f'Él tira un {hand2[oponent_second_round]}!')
         hand2.remove(hand2[oponent_first_round])
         #Third round
-        player_third_round = int(input(f'Elegí la carta que vas a tirar 1) {hand1[0]}: '))
+        player_third_round = int(input(f'Tu turno: 1) {hand1[0]}'))
         print(f'Él tira un {hand2[0]}')
 
 def shuffle(deck, hand):
